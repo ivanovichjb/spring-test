abcb8de415f77daa2de62b95fe90d792af4f076e
ivanovich juarez <lebniz04@gmail.com>
2019-09-01 14:28:41 -0500

    update spring-boot project adding controller service and entities

---
+++ src/main/java/com/example/demo/controller/InformationController.java
@@ -0,0 +1,9 @@
+package com.example.demo.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/v1/information")
+public class InformationController {
+
+}
